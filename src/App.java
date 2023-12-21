0a750cb62d6fb9da8beefb8e78c8241ee1142dad
Omkar Kadam <kadam.omkar@outlook.com>
2023-12-22 02:05:46 +0530

    Calculated Employee wage for multiple companies

--- src/App.java
+++ src/App.java
@@ -2,35 +2,24 @@ public class App {
 
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
-    public static final int EMP_RATE_PER_HOUR = 20;
     public static final int FULL_TIME_WORKING_HOURS = 8;
     public static final int PART_TIME_WORKING_HOURS = 4;
-    public static final int WORKING_DAYS_PER_MONTH = 20;
-    public static final int MAX_HRS_IN_MONTH = 100;
 
-    int totalEmpWage = 0;
-    int totalEmpHours = 0;
-
-    int fullTimeDaysWorked = 0;
-    int partTimeDaysWorked = 0;
-
-    int getWorkingHoursADay(int attendance, int totalEmpHours)
+    static int getWorkingHoursADay(int attendance, int totalEmpHours, int maxEmpHours)
     {
         int empHours = 0;
 
         switch(attendance){
                 case IS_FULL_TIME:
-                    if(totalEmpHours != MAX_HRS_IN_MONTH)
+                    if(totalEmpHours != maxEmpHours)
                     {
                         empHours = FULL_TIME_WORKING_HOURS;
-                        (fullTimeDaysWorked)++;
                     }
                     break;
                 case IS_PART_TIME:
-                    if(totalEmpHours != MAX_HRS_IN_MONTH)
+                    if(totalEmpHours != maxEmpHours)
                     {
                         empHours = PART_TIME_WORKING_HOURS;
-                        (partTimeDaysWorked)++;
                     }
                     break;
                 default:
@@ -40,41 +29,49 @@ public class App {
         return empHours;
     }
 
-    void calculateWage()
+    static int calculateWage(int empRatePerHour, int daysPerMonth, int maxEmpHours)
     {
+        int totalEmpWage = 0;
+        int totalEmpHours = 0;
+
         int currDay = 1;
-        while(currDay <= WORKING_DAYS_PER_MONTH && totalEmpHours <= MAX_HRS_IN_MONTH)
+        while(currDay <= daysPerMonth && totalEmpHours <= maxEmpHours)
         {
             int empHours = 0;
             int empWage = 0;
 
             int attendance = (int)(Math.floor(Math.random() * 3.0));
 
-            empHours = getWorkingHoursADay(attendance, totalEmpHours);
+            empHours = getWorkingHoursADay(attendance, totalEmpHours, maxEmpHours);
 
-            if(totalEmpHours + empHours > MAX_HRS_IN_MONTH)
+            if(totalEmpHours + empHours > maxEmpHours)
             {
-                empHours = MAX_HRS_IN_MONTH - totalEmpHours;
+                empHours = maxEmpHours - totalEmpHours;
             }
 
-            empWage = EMP_RATE_PER_HOUR * empHours;
+            empWage = empRatePerHour * empHours;
             totalEmpWage += empWage;
             totalEmpHours += empHours;
             currDay++;
         }
 
-        System.out.println("Full Time: " + fullTimeDaysWorked + " Days");
-        System.out.println("Part Time: " + partTimeDaysWorked + " Days");
         System.out.println("Hours Worked: " + totalEmpHours);
         System.out.println("Employee Wage for the month: " + totalEmpWage);
+
+        return totalEmpWage;
     }
 
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to Employee Wage Computation.");
+        
+        int empRatePerHourCompany1 = 20, daysPerMonthCompany1 = 20, maxEmpHoursCompany1 = 100;
+        System.out.println("For Company 1: ");
+        calculateWage(empRatePerHourCompany1, daysPerMonthCompany1, maxEmpHoursCompany1);
+        System.out.println();
 
-        App mainObj = new App();
-
-        mainObj.calculateWage();
-
+        int empRatePerHourCompany2 = 15, daysPerMonthCompany2 = 18, maxEmpHoursCompany2 = 80;
+        System.out.println("For Company 2: ");
+        calculateWage(empRatePerHourCompany2, daysPerMonthCompany2, maxEmpHoursCompany2);
+        System.out.println();
     }
 }
