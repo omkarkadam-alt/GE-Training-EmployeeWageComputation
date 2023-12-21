42f919ede52bdcf9c2e582ada997355ba859ae8e
Omkar Kadam <kadam.omkar@outlook.com>
2023-12-21 20:25:36 +0530

    Refactored code to incorporate OOPS concepts

--- src/App.java
+++ src/App.java
@@ -8,15 +8,40 @@ public class App {
     public static final int WORKING_DAYS_PER_MONTH = 20;
     public static final int MAX_HRS_IN_MONTH = 100;
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to Employee Wage Computation.");
+    int totalEmpWage = 0;
+    int totalEmpHours = 0;
 
-        int totalEmpWage = 0;
-        int totalEmpHours = 0;
+    int fullTimeDaysWorked = 0;
+    int partTimeDaysWorked = 0;
 
-        int fullTimeDaysWorked = 0;
-        int partTimeDaysWorked = 0;
+    int getWorkingHoursADay(int attendance, int totalEmpHours)
+    {
+        int empHours = 0;
 
+        switch(attendance){
+                case IS_FULL_TIME:
+                    if(totalEmpHours != MAX_HRS_IN_MONTH)
+                    {
+                        empHours = FULL_TIME_WORKING_HOURS;
+                        (fullTimeDaysWorked)++;
+                    }
+                    break;
+                case IS_PART_TIME:
+                    if(totalEmpHours != MAX_HRS_IN_MONTH)
+                    {
+                        empHours = PART_TIME_WORKING_HOURS;
+                        (partTimeDaysWorked)++;
+                    }
+                    break;
+                default:
+                    empHours = 0;
+        }
+
+        return empHours;
+    }
+
+    void calculateWage()
+    {
         int currDay = 1;
         while(currDay <= WORKING_DAYS_PER_MONTH && totalEmpHours <= MAX_HRS_IN_MONTH)
         {
@@ -25,27 +50,7 @@ public class App {
 
             int attendance = (int)(Math.floor(Math.random() * 3.0));
 
-            switch(attendance){
-                case IS_FULL_TIME:
-                    // System.out.println("Employee is working full time.");
-                    if(totalEmpHours != MAX_HRS_IN_MONTH)
-                    {
-                        empHours = FULL_TIME_WORKING_HOURS;
-                        fullTimeDaysWorked++;
-                    }
-                    break;
-                case IS_PART_TIME:
-                    // System.out.println("Employee is working part time.");
-                    if(totalEmpHours != MAX_HRS_IN_MONTH)
-                    {
-                        empHours = PART_TIME_WORKING_HOURS;
-                        partTimeDaysWorked++;
-                    }
-                    break;
-                default:
-                    // System.out.println("Employee is on leave.");
-                    empHours = 0;
-            }
+            empHours = getWorkingHoursADay(attendance, totalEmpHours);
 
             if(totalEmpHours + empHours > MAX_HRS_IN_MONTH)
             {
@@ -62,6 +67,14 @@ public class App {
         System.out.println("Part Time: " + partTimeDaysWorked + " Days");
         System.out.println("Hours Worked: " + totalEmpHours);
         System.out.println("Employee Wage for the month: " + totalEmpWage);
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to Employee Wage Computation.");
+
+        App mainObj = new App();
+
+        mainObj.calculateWage();
 
     }
 }
